c83b2802756dd60275a58e1292c5728dc7d8477e
eddie ashton <eddie.ashton@madetech.com>
2023-02-22 15:35:52 +0000

    Completed the kata

--- liftButton.js
+++ liftButton.js
@@ -1,6 +1,17 @@
 class LiftButton {
+    lightOn = false;
+    doorsOpen = false;
     isLightOn() {
-        return false;
+        return this.lightOn;
+    }
+    pressButton() {
+        if(!this.doorsOpen){
+            this.lightOn = true;
+        }
+    }
+    openDoors(){
+        this.lightOn = false
+        this.doorsOpen = true;
     }
 }
 module.exports = {LiftButton};
--- liftButton.test.js
+++ liftButton.test.js
@@ -1,4 +1,4 @@
-const {LiftButton} = require('./LiftButton.js')
+const {LiftButton} = require('./LiftButton')
 
 
 describe('A test suite for the lift button kata', () => {
@@ -7,6 +7,34 @@ describe('A test suite for the lift button kata', () => {
     })
     test('is the light on', () => {
         let liftButton = new LiftButton();
+        expect(liftButton.isLightOn()).toEqual(false);
+    })
+    test('does the light go on when the button is pressed', () => {
+        let liftButton = new LiftButton();
+        expect(liftButton.isLightOn()).toEqual(false);
+        liftButton.pressButton();
         expect(liftButton.isLightOn()).toEqual(true);
+    })
+    test('if the light is on the button doesn\'t change the state', () => {
+        let liftButton = new LiftButton();
+        expect(liftButton.isLightOn()).toEqual(false);
+        liftButton.pressButton();
+        expect(liftButton.isLightOn()).toEqual(true);
+        liftButton.pressButton();
+        expect(liftButton.isLightOn()).toEqual(true);
+    })
+    test('when the doors open the light goes off', () => {
+        let liftButton = new LiftButton();
+        liftButton.pressButton();
+        expect(liftButton.isLightOn()).toEqual(true);
+        liftButton.openDoors();
+        expect(liftButton.isLightOn()).toEqual(false);
+    })
+    test('if the doors are open, pressing the button will not make the light go on', () => {
+        let liftButton = new LiftButton();
+        liftButton.openDoors();
+        expect(liftButton.isLightOn()).toEqual(false);
+        liftButton.pressButton();
+        expect(liftButton.isLightOn()).toEqual(false);
     })
 })
